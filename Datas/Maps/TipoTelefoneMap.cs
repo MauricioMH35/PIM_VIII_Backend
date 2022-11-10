013a1e90cc1443e2310aeba711e48ff8cb36e74c
Mauricio Mityo Hidani <mauriciomh35@gmail.com>
2022-11-09 23:20:45 -0300

    feat: mapeamento tipo telefone do contexto usado no banco de dados

---
+++ Datas/Maps/TipoTelefoneMap.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PIM_VIII.Models;
+
+namespace PIM_VIII.Datas.Maps {
+    public class TipoTelefoneMap : IEntityTypeConfiguration<TipoTelefone> {
+
+        public void Configure(EntityTypeBuilder<TipoTelefone> builder) {
+            builder.HasKey(x => x.id);
+            builder.Property(x => x.tipo)
+                .IsRequired(true)
+                .HasMaxLength(10);
+        }
+
+    }
+}
