b1d86f9741454c8bc88d5c0e053d3d620e5192fa
Mauricio Mityo Hidani <mauriciomh35@gmail.com>
2022-11-09 23:17:48 -0300

    feat: interface do repository telefone

---
+++ Repositories/Interfaces/ITelefoneRepository.cs
@@ -0,0 +1,15 @@
+﻿using PIM_VIII.Models;
+
+namespace PIM_VIII.Repositories.Interfaces {
+    public interface ITelefoneRepository {
+
+        Task<List<Telefone>> EncontrarTodos();
+        Task<List<Telefone>> EncontrarPorDdd(int ddd);
+        Task<Telefone> EncontrarPorNumero(int numero);
+        Task<Telefone> EncontrarPorId(int id);
+        Task<bool> Adicionar(Telefone telefone);
+        Task<bool> Atualizar(int id, Telefone telefone);
+        Task<bool> Remover(int id);
+
+    }
+}
