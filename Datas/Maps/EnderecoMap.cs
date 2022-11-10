0c3ac87658de0b263123d219f61041f74a9a754f
Mauricio Mityo Hidani <mauriciomh35@gmail.com>
2022-11-09 23:19:46 -0300

    feat: mapeamento endereco do contexto usado no banco de dados

---
+++ Datas/Maps/EnderecoMap.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PIM_VIII.Models;
+
+namespace PIM_VIII.Datas.Maps {
+    public class EnderecoMap : IEntityTypeConfiguration<Endereco> {
+
+        public void Configure(EntityTypeBuilder<Endereco> builder) {
+            builder.HasKey(x => x.id);
+            builder.Property(x => x.logradouro)
+                .HasMaxLength(256);
+            builder.Property(x => x.numero)
+                .IsRequired(true);
+            builder.Property(x => x.cep)
+                .IsRequired(true);
+            builder.Property(x => x.bairro)
+                .HasMaxLength(100);
+            builder.Property(x => x.cidade)
+                .HasMaxLength(100);
+            builder.Property(x => x.estado)
+                .HasMaxLength(2);
+        }
+
+    }
+}
