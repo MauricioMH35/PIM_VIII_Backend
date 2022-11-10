1a8a63911d011e857731701136793aa839e7535b
Mauricio Mityo Hidani <mauriciomh35@gmail.com>
2022-11-09 23:21:52 -0300

    feat: mapeamento do contexto

---
+++ Datas/BancoDadosContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PIM_VIII.Datas.Maps;
+using PIM_VIII.Models;
+
+namespace PIM_VIII.Datas {
+    public class BancoDadosContext : DbContext {
+
+        public BancoDadosContext(DbContextOptions options) : base(options) {
+            
+        }
+
+        public DbSet<Pessoa> Pessoas { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+        public DbSet<Telefone> Telefones { get; set; }
+        public DbSet<TipoTelefone> TipoTelefones { get; set; }
+        public DbSet<PessoaTelefone> PessoaTelefones { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            modelBuilder.ApplyConfiguration(new PessoaMap());
+            modelBuilder.ApplyConfiguration(new EnderecoMap());
+            modelBuilder.ApplyConfiguration(new TelefoneMap());
+            modelBuilder.ApplyConfiguration(new TipoTelefoneMap());
+            modelBuilder.ApplyConfiguration(new PessoaTelefoneMap());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
