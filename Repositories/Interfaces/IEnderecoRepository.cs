14ff6559d9d78132624dc27d92f634359e707389
Mauricio Mityo Hidani <mauriciomh35@gmail.com>
2022-11-09 23:17:27 -0300

    feat: interface do repository endereco

---
+++ Repositories/Interfaces/IEnderecoRepository.cs
@@ -0,0 +1,14 @@
+﻿using PIM_VIII.Models;
+
+namespace PIM_VIII.Repositories.Interfaces {
+    public interface IEnderecoRepository {
+
+        Task<List<Endereco>> EncontrarTodos();
+        Task<List<Endereco>> EncontrarPorCep(string cep);
+        Task<Endereco> EncontrarPorId(int id);
+        Task<bool> Adicionar(Endereco endereco);
+        Task<bool> Atualizar(int id, Endereco endereco);
+        Task<bool> Remover(int id);
+
+    }
+}
