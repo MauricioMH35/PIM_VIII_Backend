1f131e58aad99cca63af57684474ae3ab82a71aa
Mauricio Mityo Hidani <mauriciomh35@gmail.com>
2022-11-09 23:18:17 -0300

    feat interface do repository tipo telefone

---
+++ Repositories/Interfaces/ITipoTelefoneRepository.cs
@@ -0,0 +1,15 @@
+﻿using PIM_VIII.Models;
+
+namespace PIM_VIII.Repositories.Interfaces {
+
+    public interface ITipoTelefoneRepository {
+
+        Task<List<TipoTelefone>> EncontrarTodos();
+        Task<TipoTelefone> EncontrarPorId(int id);
+        Task<bool> Adicionar(TipoTelefone tipoTelefone);
+        Task<bool> Atualizar(int id, TipoTelefone tipoTelefone);
+        Task<bool> Remover(int id);
+
+    }
+
+}
