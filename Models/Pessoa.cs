b1bb5894a0cf1355e2a5e2358d7398ae5689ef95
Mauricio Mityo Hidani <mauriciomh35@gmail.com>
2022-11-09 23:11:30 -0300

    feat: entidade pessoa

---
+++ Models/Pessoa.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PIM_VIII.Models {
+    
+    [Table("TB_PESSOAS")] 
+    public class Pessoa {
+
+        public int id { get; set; }
+        public string nome { get; set; }
+        public long cpf { get; set; }
+        public int? enderecoId { get; set; }
+        public virtual Endereco? endereco { get; set; }
+
+        private Pessoa() {
+        }
+
+        public Pessoa(int id, string nome, long cpf, int? enderecoId, Endereco? endereco) {
+            this.id = id;
+            this.nome = nome;
+            this.cpf = cpf;
+            this.enderecoId = enderecoId;
+            this.endereco = endereco;
+        }
+
+    }
+
+}
