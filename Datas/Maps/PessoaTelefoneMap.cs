a66d945354477380fc5d634805dd6ce293df787f
Mauricio Mityo Hidani <mauriciomh35@gmail.com>
2022-11-09 23:21:23 -0300

    feat: mapeamento relacionamento pessoa telefone do contexto usado no banco de dados

---
+++ Datas/Maps/PessoaTelefoneMap.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PIM_VIII.Models;
+
+namespace PIM_VIII.Datas.Maps {
+    public class PessoaTelefoneMap : IEntityTypeConfiguration<PessoaTelefone> {
+
+        public void Configure(EntityTypeBuilder<PessoaTelefone> builder) {
+            builder.HasKey(x => x.id);
+            builder.HasOne(x => x.pessoa).WithOne();
+            builder.HasMany(x => x.telefones);
+        }
+
+    }
+}
