843c6e483c617a5f4e1299746741e71cfae17544
Mauricio Mityo Hidani <mauriciomh35@gmail.com>
2022-11-09 23:13:30 -0300

    feat: entidade representativa relacionamento pessoa telefone

---
+++ Models/PessoaTelefone.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PIM_VIII.Models {
+
+    [Table("TB_PESSOAS_TELEFONES")]
+    public class PessoaTelefone {
+
+        public int id { get; set; }
+        public int? pessoaId { get; set; }
+        public Pessoa pessoa { get; set; }
+        public int? telefoneId { get; set; }
+        public List<Telefone> telefones { get; set; }
+
+        private PessoaTelefone() {
+        }
+
+        public PessoaTelefone(int id, int pessoaId, Pessoa pessoa, int telefoneId, 
+            List<Telefone> telefones) {
+            this.id = id;
+            this.pessoaId = pessoaId;
+            this.pessoa = pessoa;
+            this.telefoneId = telefoneId;
+            this.telefones = telefones;
+        }
+
+    }
+
+}
