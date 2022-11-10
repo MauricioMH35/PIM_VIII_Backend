396c0b9352c963606dd4a4f75923f6c8cdff92be
Mauricio Mityo Hidani <mauriciomh35@gmail.com>
2022-11-09 23:17:06 -0300

    feat: interface do repository pessoa

---
+++ Repositories/Interfaces/IPessoaRepository.cs
@@ -0,0 +1,14 @@
+﻿using PIM_VIII.Models;
+
+namespace PIM_VIII.Repositories.Interfaces {
+    public interface IPessoaRepository {
+        
+        Task<List<Pessoa>> EncontrarTodos();
+        Task<Pessoa> EncontrarPorId(int id);
+        Task<Pessoa> EncontrarPorCpf(long cpf);
+        Task<bool> Adicionar(Pessoa pessoa);
+        Task<bool> Atualizar(int id, Pessoa pessoa);
+        Task<bool> Remover(int id);
+
+    }
+}
