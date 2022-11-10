b1423f23a381ae576e0994d3c2fd3b7dee9b7b85
Mauricio Mityo Hidani <mauriciomh35@gmail.com>
2022-11-09 23:11:49 -0300

    feat: entidade endereco

---
+++ Models/Endereco.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PIM_VIII.Models {
+
+    [Table("TB_ENDERECOS")]
+    public class Endereco {
+
+        public int id { get; set; }
+        public string? logradouro { get; set; }
+        public int numero { get; set; }
+        public string cep { get; set; }
+        public string? bairro { get; set; }
+        public string? cidade { get; set; }
+        public string? estado { get; set; }
+
+        private Endereco() { 
+        
+        }
+
+        public Endereco(int id, string? logradouro, int numero, string cep, string? bairro, 
+            string? cidade, string? estado) {
+            this.id = id;
+            this.logradouro = logradouro;
+            this.numero = numero;
+            this.cep = cep;
+            this.bairro = bairro;
+            this.cidade = cidade;
+            this.estado = estado;
+        }
+
+    }
+
+}
