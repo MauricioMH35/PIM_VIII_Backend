026089d94b32d09a3445a4df796d4370d7ed7980
Mauricio Mityo Hidani <mauriciomh35@gmail.com>
2022-11-09 23:12:12 -0300

    feat: entidade telefone

---
+++ Models/Telefone.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PIM_VIII.Models {
+
+    [Table("TB_TELEFONES")]
+    public class Telefone {
+    
+        public int id { get; set; }
+        public int ddd { get; set; }
+        public int numero { get; set; }
+        public int? tipoId { get; set; }
+        public TipoTelefone? tipo { get; set; }
+
+        private Telefone() {
+        }
+
+        public Telefone(int id, int ddd, int numero, int? tipoId, TipoTelefone tipo) {
+            this.id = id;
+            this.ddd = ddd;
+            this.numero = numero;
+            this.tipoId = tipoId;
+            this.tipo = tipo;
+        }
+
+    }
+
+}
