bfe76d53c274a2db7500e7293987ab46110003a6
Mauricio Mityo Hidani <mauriciomh35@gmail.com>
2022-11-09 23:19:22 -0300

    feat: mapeamento pesso do contexto usado no banco de dados

---
+++ Datas/Maps/PessoaMap.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PIM_VIII.Models;
+
+namespace PIM_VIII.Datas.Maps {
+    public class PessoaMap : IEntityTypeConfiguration<Pessoa> {
+
+        public void Configure(EntityTypeBuilder<Pessoa> builder) {
+            builder.HasKey(x => x.id);
+            builder.Property(x => x.nome).HasMaxLength(128);
+            builder.Property(x => x.cpf).IsRequired().HasMaxLength(11);
+            builder.HasIndex(x => x.cpf).IsUnique();
+            builder.HasOne(x => x.endereco);
+        }
+
+    }
+}
