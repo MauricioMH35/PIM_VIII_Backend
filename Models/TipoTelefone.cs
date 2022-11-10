4bada232873fe896a0516eeacf4e71d8cf045a34
Mauricio Mityo Hidani <mauriciomh35@gmail.com>
2022-11-09 23:12:53 -0300

    feat: entidade tipo de telefone

---
+++ Models/TipoTelefone.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PIM_VIII.Models {
+
+    [Table("TB_TIPOS_TELEFONES")]
+    public class TipoTelefone {
+
+        public int id { get; set; }
+        public string tipo { get; set; }
+
+        private TipoTelefone() {
+        }
+
+        public TipoTelefone(int id, string? tipo) {
+            this.id = id;
+            this.tipo = tipo;
+        }
+
+    }
+
+}
